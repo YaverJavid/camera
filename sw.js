d09f5d014c175751f5299a2c28bfb859010c4120
Yaver Javid <81960873+YaverJavid@users.noreply.github.com>
2022-11-20 20:48:46 +0530

    2.0 --is-req-- --omit--

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll([
+                "./",
+                "images/favicon.png",
+                "main.js",
+                "utils.js",
+                "filters.js",
+                "camera.js",
+                "audio/camera-13695.wav",
+                "style.css"
+                ])
+        })
+    )
+})
+
+self.addEventListener("fetch",e =>{
+    console.log('request_handle')
+    e.respondWith(
+        caches.match(e.request).then(res =>{
+            return res || fetch(e.request)
+        })
+        )
+})
